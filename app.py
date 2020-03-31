cba4e14aa16a78275c2440b636ee29c57522e90f
imo-tikuwa <imotikuwabathclean@gmail.com>
2020-03-31 21:37:28 +0900

    ロガーを入れたけどよくわかってない。ぐだぐだ

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
 from flask import Flask, render_template, request
+import logzero
+from logzero import logger
 import io
+from io import StringIO
+import csv
+import sys
+import json
+
+CSV_INDEX_DIFFICULTY = 0
+CSV_INDEX_SCORE = 1
+CSV_INDEX_REMAIN = 2
+CSV_INDEX_GRAZE = 3
+CSV_INDEX_CURRET = 7
+
+
+# ログ出力設定 (application.logを出力、標準出力は行わない)
+logzero.logfile('log/application.log', disableStderrLogger = True)
 
 app = Flask(__name__, static_folder = 'assets')
 # 10MBまでアップロード可
@@ -17,23 +33,46 @@ def disp_chart():
     # グラフ表示
 
     # CSVファイル1は必須
-#     if 'csv_file1' not in request.files:
-#         return render_template("index.html")
-#
-#     csv_file1 = request.files.get('csv_file1')
-#     if csv_file1.mimetype != 'text/csv':
-#         return render_template("index.html")
-#
-#     text_stream = io.TextIOWrapper(csv_file1.stream, encoding = 'cp932')
-#     for row in csv.reader(text_stream):
-#         print(row)
+    if 'csv_file1' not in request.files:
+        return render_template("index.html", error = 'CSVファイル1は必須です。')
 
+    csv_file1 = request.files.get('csv_file1')
+    if csv_file1.mimetype not in ['text/csv', 'application/vnd.ms-excel']:
+        return render_template("index.html", error = 'CSVファイル1の形式が正しくありません。')
 
+    # 文字エンコーディングがsjisのストリームを取得（ここでutf8にする方法が不明）
+    text_stream = io.TextIOWrapper(csv_file1, encoding = 'sjis')
+    text_data = StringIO(text_stream.read())
 
+    index = 0
+    score_data = {}
+    graze_data = {}
+    remain_data = {}
+    for row in csv.reader(text_data):
 
+        # csvの1行をsjis → utf-8変換？ググってもよくわからず
+        row = [x.encode('utf8').decode('utf8') for x in row]
 
+        if index == 0:
+            if row != ['難易度', 'スコア', '残機', 'グレイズ', 'ボス', 'ボス残機', 'スペル', '現在地']:
+                # ヘッダ行チェック
+                return render_template("index.html", error = 'CSVファイル1のヘッダ行が正しくありません。')
+        else:
+            # データ行を辞書に追加(Python3.7からは辞書に入れた順番が保持される模様)
+            current = row[CSV_INDEX_CURRET]
+            score_data[current] = row[CSV_INDEX_SCORE]
+            graze_data[current] = row[CSV_INDEX_GRAZE]
+            remain_data[current] = row[CSV_INDEX_REMAIN]
+        index += 1
 
-    return render_template("charts.html")
+    logger.debug(score_data)
+    logger.debug(graze_data)
+    logger.debug(remain_data)
+    logger.debug(json.dumps(score_data))
+    logger.debug(json.dumps(graze_data))
+    logger.debug(json.dumps(remain_data))
+
+    return render_template("charts.html", score = json.dumps(score_data), graze = json.dumps(graze_data), remain = json.dumps(remain_data))
 
 
 if __name__ == "__main__":
