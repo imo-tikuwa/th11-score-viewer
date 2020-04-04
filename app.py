0db455f6224f1bd7d30ae8849bddf5ed61182f77
imo-tikuwa <imotikuwabathclean@gmail.com>
2020-04-04 18:37:55 +0900

    CSVファイル1とCSVファイル2を比較するため、処理を修正中

--- app.py
+++ app.py
@@ -12,7 +12,10 @@ CSV_INDEX_DIFFICULTY = 0
 CSV_INDEX_SCORE = 1
 CSV_INDEX_REMAIN = 2
 CSV_INDEX_GRAZE = 3
-CSV_INDEX_CURRET = 7
+CSV_INDEX_CURRENT = 7
+
+CSV1_INDEX = 0
+CSV2_INDEX = 1
 
 
 # ログ出力設定 (application.logを出力、標準出力は行わない)
@@ -46,15 +49,22 @@ def disp_chart():
     if csv_file1.mimetype not in ['text/csv', 'application/vnd.ms-excel']:
         return render_template("index.html", error = 'CSVファイル1の形式が正しくありません。')
 
-    # 文字エンコーディングがsjisのストリームを取得（ここでutf8にする方法が不明）
-    text_stream = io.TextIOWrapper(csv_file1, encoding = 'sjis')
-    text_data = StringIO(text_stream.read())
+    # CSVファイル2を取得
+    csv_file2 = None
+    logger.debug(request.files)
+    if 'csv_file2' in request.files and request.files['csv_file2'].filename != '':
+        csv_file2 = request.files.get('csv_file2')
+        if csv_file2.mimetype not in ['text/csv', 'application/vnd.ms-excel']:
+            return render_template("index.html", error = 'CSVファイル2の形式が正しくありません。')
 
-    index = 0
-    score_data = {}
-    graze_data = {}
-    remain_data = {}
-    for row in csv.reader(text_data):
+    # 文字エンコーディングがsjisのストリームを取得（ここでutf8にする方法が不明）
+    csv_stream1 = io.TextIOWrapper(csv_file1, encoding = 'sjis')
+    csv_data1 = StringIO(csv_stream1.read())
+
+    score_data = {CSV1_INDEX: {}}
+    graze_data = {CSV1_INDEX: {}}
+    remain_data = {CSV1_INDEX: {}}
+    for index, row in enumerate(csv.reader(csv_data1)):
 
         # csvの1行をsjis → utf-8変換？ググってもよくわからず
         row = [x.encode('utf8').decode('utf8') for x in row]
@@ -65,20 +75,52 @@ def disp_chart():
                 return render_template("index.html", error = 'CSVファイル1のヘッダ行が正しくありません。')
         else:
             # データ行を辞書に追加(Python3.7からは辞書に入れた順番が保持される模様)
-            current = row[CSV_INDEX_CURRET]
+            current = row[CSV_INDEX_CURRENT]
             if current == '':
                 current = str(index)
-            score_data[current] = row[CSV_INDEX_SCORE]
-            graze_data[current] = row[CSV_INDEX_GRAZE]
-            remain_data[current] = row[CSV_INDEX_REMAIN]
-        index += 1
+            score_data[CSV1_INDEX][current] = row[CSV_INDEX_SCORE]
+            graze_data[CSV1_INDEX][current] = row[CSV_INDEX_GRAZE]
+            remain_data[CSV1_INDEX][current] = row[CSV_INDEX_REMAIN]
+
+
+    # CSVファイル2が存在するとき1と2が同じ難易度か判定する。違ったら比較は不可とする
+    if csv_file2 is not None:
+        csv_stream2 = io.TextIOWrapper(csv_file2, encoding = 'sjis')
+        csv_data2 = StringIO(csv_stream2.read())
+
+        score_data[CSV2_INDEX] = {}
+        graze_data[CSV2_INDEX] = {}
+        remain_data[CSV2_INDEX] = {}
+
+        # CSVファイル1の現在地と一致するデータのみ、表示用の辞書データに追加する
+        # そのため、CSVファイル1の現在地のリストを取得する
+        csv1_currents = score_data[CSV1_INDEX].keys()
+
+        for index, row in enumerate(csv.reader(csv_data2)):
+            row = [x.encode('utf8').decode('utf8') for x in row]
+            if index == 0:
+                if row != ['難易度', 'スコア', '残機', 'グレイズ', 'ボス', 'ボス残機', 'スペル', '現在地']:
+                    return render_template("index.html", error = 'CSVファイル2のヘッダ行が正しくありません。')
+            else:
+                current = row[CSV_INDEX_CURRENT]
+                if current == '':
+                    continue
+                elif not current in csv1_currents:
+                    logger.debug("次のキーはCSVファイル1側に存在しないためスキップ：" + current)
+                    continue
+
+                # 比較対象データ(CSVファイル2)の現在地を元にCSVファイル1の現在地のインデックスを取得、空でないときだけセット
+                score_data[CSV2_INDEX][current] = row[CSV_INDEX_SCORE]
+                graze_data[CSV2_INDEX][current] = row[CSV_INDEX_GRAZE]
+                remain_data[CSV2_INDEX][current] = row[CSV_INDEX_REMAIN]
+
 
     logger.debug(score_data)
 #     logger.debug(graze_data)
 #     logger.debug(remain_data)
     logger.debug(json.dumps(score_data, ensure_ascii=False))
-#     logger.debug(json.dumps(graze_data))
-#     logger.debug(json.dumps(remain_data))
+#     logger.debug(json.dumps(graze_data, ensure_ascii=False))
+#     logger.debug(json.dumps(remain_data, ensure_ascii=False))
 
     return render_template("charts.html",
                            score = json.dumps(score_data, ensure_ascii=False),
